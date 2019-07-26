5e5f4101b1679958ac2346568ba45942ad35eec7
jlim2 <jeffreylim@outlook.com>
2019-07-26 14:20:45 -0400

    adding some initializers

--- app/src/main/java/com/example/baseballscore/MainActivity.java
+++ app/src/main/java/com/example/baseballscore/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.baseballscore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button strike;
+    Button ball;
+    Button out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
